a97711814c820fc60af722e998521aa3a75f7ab8
Alexandr <dunkelpunk@gmail.com>
2023-03-19 15:07:13 +0300

    zadanit2

---
+++ zadacha2/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+
+
+int f(int n)
+{
+    if (n == 0)
+        return 1;
+ int count = 0;
+ while (n > 0)
+ {
+    n/=10;
+    count ++;
+ }
+ return count;
+}
+
+Console.Clear();
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(f(n));
