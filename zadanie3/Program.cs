2610f5404bb5bcf8726157c8704f2d144a891ca3
Alexandr <dunkelpunk@gmail.com>
2023-03-19 15:25:04 +0300

    domashnee zadanit3

---
+++ zadanie3/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+
+
+
+int f(int n)
+{
+
+    int res = 1;
+    for (int i = 2; i <= n; i++)
+    res *= i;
+    return res;
+}
+
+Console.Clear();
+Console.Write("введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(f(number));
