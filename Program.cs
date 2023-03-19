a70d4e6b74c2c67b776ef3d6cb1c78d94733afca
Alexandr <dunkelpunk@gmail.com>
2023-03-19 14:49:02 +0300

    practicum4

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿//задача 24: Напишите программу, которая принимает на вход число (А)
+// и выдает сумму чисел от 1 до А.
+// 7->28
+// 4->10
+// 8->36
+
+double f(idouble  n)
+{
+    return ((1+n)/2*n);
+}
+
+Console.Clear();
+double  a = Convert.ToDouble (Console.ReadLine());
+Console.WriteLine(f(a));
